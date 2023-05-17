3cc45eba72c2b98a3a17951869baa0673a6c49e6
Marko Mušica <markomusica@gmail.com>
2023-05-17 23:43:02 +0200

    Refactored and cleaned project, added .gitignore for Target

--- src/main/java/com/github/mmusica/interfaces/GeneralRepository.java
+++ src/main/java/com/github/mmusica/interfaces/GeneralRepository.java
@@ -1,20 +1,9 @@
 package com.github.mmusica.interfaces;
 
 public interface GeneralRepository<T> {
-
-
     public T add(T entity);
-
     public  T find(Long id);
-
-
     public T update(T entity, Long id);
-
-
     public void delete(T entity);
-
-
     public void close();
-
-
 }
